446a0b74dc5daf3cf67e3fff01c7c2a5f13af569
Keval <keval.langalia@live.com>
2021-02-19 04:08:17 +0530

    just reformatted

--- app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
@@ -22,19 +22,19 @@ fun parseAsteroidsJsonResult(jsonResult: JSONObject): ArrayList<Asteroid> {
             val codename = asteroidJson.getString("name")
             val absoluteMagnitude = asteroidJson.getDouble("absolute_magnitude_h")
             val estimatedDiameter = asteroidJson.getJSONObject("estimated_diameter")
-                .getJSONObject("kilometers").getDouble("estimated_diameter_max")
+                    .getJSONObject("kilometers").getDouble("estimated_diameter_max")
 
             val closeApproachData = asteroidJson
-                .getJSONArray("close_approach_data").getJSONObject(0)
+                    .getJSONArray("close_approach_data").getJSONObject(0)
             val relativeVelocity = closeApproachData.getJSONObject("relative_velocity")
-                .getDouble("kilometers_per_second")
+                    .getDouble("kilometers_per_second")
             val distanceFromEarth = closeApproachData.getJSONObject("miss_distance")
-                .getDouble("astronomical")
+                    .getDouble("astronomical")
             val isPotentiallyHazardous = asteroidJson
-                .getBoolean("is_potentially_hazardous_asteroid")
+                    .getBoolean("is_potentially_hazardous_asteroid")
 
             val asteroid = Asteroid(id, codename, formattedDate, absoluteMagnitude,
-                estimatedDiameter, relativeVelocity, distanceFromEarth, isPotentiallyHazardous)
+                    estimatedDiameter, relativeVelocity, distanceFromEarth, isPotentiallyHazardous)
             asteroidList.add(asteroid)
         }
     }
