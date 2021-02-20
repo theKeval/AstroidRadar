cac8c2369ae7575c1793c7f39820f89b403e6280
Keval <keval.langalia@live.com>
2021-02-20 07:35:53 +0530

    recyclerview adapter related code && empty layout file for asteroid recyclerview

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -2,19 +2,24 @@ package com.udacity.asteroidradar.main
 
 import android.os.Bundle
 import android.view.*
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.R
+import com.udacity.asteroidradar.databinding.AsteroidViewBinding
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
+import com.udacity.asteroidradar.models.Asteroid
 
 class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onViewCreated()"
+        }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        ViewModelProvider(this, MainViewModel.Factory(activity.application)).get(MainViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +34,10 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.main_overflow_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
@@ -37,4 +46,47 @@ class MainFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return true
     }
+}
+
+
+class AsteroidsAdapter(val callback: AsteroidClicked): RecyclerView.Adapter<AsteroidViewHolder>() {
+
+    var asteroids: List<Asteroid> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AsteroidViewHolder {
+        val withDataBinding: AsteroidViewBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            AsteroidViewHolder.LAYOUT,
+            parent,
+            false)
+
+        return AsteroidViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: AsteroidViewHolder, position: Int) {
+        holder.binding.also {
+            it.asteroid = asteroids[position]
+            it.asteroidCallback = callback
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return asteroids.size
+    }
+
+}
+
+class AsteroidViewHolder(val binding: AsteroidViewBinding): RecyclerView.ViewHolder(binding.root) {
+    companion object {
+        @LayoutRes
+        val LAYOUT = R.layout.asteroid_view
+    }
+}
+
+class AsteroidClicked(val block: (Asteroid) -> Unit) {
+    fun onClicked(model: Asteroid) = block(model)
 }
