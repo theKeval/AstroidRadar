dd1f7e1571c48077510e460113e2fac5cc0a5b3c
Keval <keval.langalia@live.com>
2021-02-20 19:20:10 +0530

    extension function .toPodEntity

--- app/src/main/java/com/udacity/asteroidradar/models/PictureOfDay.kt
+++ app/src/main/java/com/udacity/asteroidradar/models/PictureOfDay.kt
@@ -1,10 +1,24 @@
 package com.udacity.asteroidradar.models
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import com.udacity.asteroidradar.db.PodEntity
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class PictureOfDay(
     @Json(name = "media_type")
     val mediaType: String,
     val title: String,
     val url: String
-)
+): Parcelable
+
+
+fun PictureOfDay.toPodEntity(): PodEntity {
+    return PodEntity(
+        url = this.url,
+        title = this.title,
+        mediaType = this.mediaType
+    )
+}
