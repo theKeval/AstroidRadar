a205ab8a7d24928e66710761c2960343921ec80f
Keval <keval.langalia@live.com>
2021-02-20 07:07:37 +0530

    added api key value and completed repository code

--- app/src/main/java/com/udacity/asteroidradar/repository/AsteroidRepository.kt
+++ app/src/main/java/com/udacity/asteroidradar/repository/AsteroidRepository.kt
@@ -3,12 +3,15 @@ package com.udacity.asteroidradar.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import com.udacity.asteroidradar.api.AsteroidApi
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
 import com.udacity.asteroidradar.db.AsteroidDb
 import com.udacity.asteroidradar.db.asAsteroids
 import com.udacity.asteroidradar.models.Asteroid
+import com.udacity.asteroidradar.models.asAsteroidEntity
 import com.udacity.asteroidradar.utils.Constants
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -28,7 +31,11 @@ class AsteroidRepository(private val database: AsteroidDb) {
             val response = AsteroidApi.retrofitService.getAsteroids(
                 startDate = today,
                 endDate = today
-            )
+                // add apiKey value here
+            ).await()
+
+            val asteroids = parseAsteroidsJsonResult(JSONObject(response))
+            database.asteroidDao.insertAll(*asteroids.asAsteroidEntity().toTypedArray())
         }
     }
 
