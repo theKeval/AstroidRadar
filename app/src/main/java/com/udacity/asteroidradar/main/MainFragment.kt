f1c787394fbdfa291ea7d7977ee909a309b5437c
Keval <keval.langalia@live.com>
2021-02-24 03:22:40 +0530

    started changing style - done with main frag and asteroid view

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -1,17 +1,13 @@
 package com.udacity.asteroidradar.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import android.widget.Switch
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.R
@@ -22,6 +18,8 @@ import com.udacity.asteroidradar.repository.AsteroidFilter
 
 class MainFragment : Fragment() {
 
+    private lateinit var binding: FragmentMainBinding
+    private var asteroidAdapter: AsteroidsAdapter? = null
     private val viewModel: MainViewModel by lazy {
         val activity = requireNotNull(this.activity) {
             "You can only access the viewModel after onViewCreated()"
@@ -33,13 +31,11 @@ class MainFragment : Fragment() {
         ).get(MainViewModel::class.java)
     }
 
-    private var asteroidAdapter: AsteroidsAdapter? = null
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentMainBinding.inflate(inflater)
+        binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
@@ -74,6 +70,11 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // adding content description dynamically to provide the picture of day title in it
+        binding.activityMainImageOfTheDayLayout.contentDescription =
+            resources.getString(R.string.nasa_picture_of_day_content_description, viewModel.pod.value?.title)
+
+        // Observing the asteroids live data to reassign the list of asteroids when it changes
         viewModel.asteroids.observe(viewLifecycleOwner, Observer { asteroids ->
             asteroids?.apply {
                 asteroidAdapter?.asteroids = this
